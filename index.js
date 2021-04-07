47225ed56e06fcf5d554de383e1112fa32e9b304
Liam Heywood <heychy@Liams-MBP.home>
2021-04-07 20:18:00 +0200

    commit

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ const sass = require('node-sass');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'))
 
-  
-  app.set('views', path.join(__dirname, 'views'))
-  app.set('view engine', 'ejs')
-  app.get('/', (req, res) => res.render('pages/index'))
-  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('pages/index'))
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
